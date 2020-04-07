c007cd001a7c7071a658ea65ad050c58ea4589bc
Ilnur Gabdushev <i.gabdushev@xsolla.com>
2020-04-07 21:55:17 +0500

    replace double quotes to single quotes

--- homework-01/server.js
+++ homework-01/server.js
@@ -1,12 +1,12 @@
 function main() {
   const tree = {
-    "name": 1,
-    "items": [{
-      "name": 2,
-      "items": [{"name": 3, "items": [{"name": 8},{"name": 9},{"name": 10}]}, {"name": 4}]
+    'name': 1,
+    'items': [{
+      'name': 2,
+      'items': [{'name': 3, 'items': [{'name': 8},{'name': 9},{'name': 10}]}, {'name': 4}]
     }, {
-      "name": 5,
-      "items": [{"name": 6, "items": [{"name": 7}]}]
+      'name': 5,
+      'items': [{'name': 6, 'items': [{'name': 7}]}]
     }]
   };
   outputNode(tree);
